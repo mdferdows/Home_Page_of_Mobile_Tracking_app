4b79ebd4a5bbee8eb431516d4691d7e6499708ca
hp <ferdowsjust16@gmail.com>
2019-03-02 23:07:36 +0600

    layout

--- app/src/main/java/com/example/hp/home_page_of_mobile_tracking_app/MainActivity.java
+++ app/src/main/java/com/example/hp/home_page_of_mobile_tracking_app/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.hp.home_page_of_mobile_tracking_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button LoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        LoginButton = findViewById(R.id.LoginButtonId);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
